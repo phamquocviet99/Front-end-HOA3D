822363fc2a2e1effd6f7bc8a57a2ca18e19b31bd
phamquocviet99 <phamquocviet1211999@gmail.com>
2022-11-23 15:36:40 +0700

    3:36 23/11/2022

--- src/components/SidebarGarden/SidebarGarden.jsx
+++ src/components/SidebarGarden/SidebarGarden.jsx
@@ -7,6 +7,8 @@ import { BiBox, BiBarChartSquare, BiFoodMenu } from "react-icons/bi";
 import { GoCreditCard } from "react-icons/go";
 import { TbDatabaseExport } from "react-icons/tb";
 import { TbApps } from "react-icons/tb";
+import { MdOutlineNotificationsNone } from "react-icons/md";
+import { BiMessageRoundedDetail } from "react-icons/bi";
 
 const SidebarGarden = ({ children }) => {
   const [isShowing, setIsShowing] = useState(true);
@@ -28,186 +30,234 @@ const SidebarGarden = ({ children }) => {
     { name: "Settings", href: "#" },
     { name: "Sign out", href: "#" },
   ];
+  const sideBar = [
+    {
+      name: "Trang chủ",
+      link: "/trang-nha-vuon/trang-chu",
+      icon: <TbApps />,
+    },
+    {
+      name: "Sản phẩm",
+      link: "/trang-nha-vuon/san-pham",
+      icon: <BiBox />,
+    },
+    {
+      name: "Sản lượng",
+      link: "/trang-nha-vuon/san-luong",
+      icon: <TbDatabaseExport />,
+    },
+    {
+      name: "Hóa đơn",
+      link: "/trang-nha-vuon/don-hang",
+      icon: <BiFoodMenu />,
+    },
+    {
+      name: "Doanh thu",
+      link: "/trang-nha-vuon/doanh-thu",
+      icon: <BiBarChartSquare />,
+    },
+    {
+      name: "Ngân hàng",
+      link: "/trang-nha-vuon/ngan-hang",
+      icon: <GoCreditCard />,
+    },
+  ];
   function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
   }
   return (
-    <div className="">
-      <div className="min-h-full">
-        <Disclosure as="nav" className="bg-green-600">
-          {({ open }) => (
-            <>
-              <div className=" px-4 sm:px-6 lg:px-8">
-                <div className="flex h-16 items-center justify-between">
-                  <div className="-mr-2 flex md:hidden">
-                    {/* Mobile menu button */}
-                    <Disclosure.Button
-                      onClick={() => setIsShowing((isShowing) => !isShowing)}
-                      className="inline-flex items-center justify-center rounded-md border border-white p-2 text-white hover:bg-gray-400 hover:text-white "
+    <div className="min-h-full p-4 ">
+      <Disclosure as="nav" className="back-ground-header rounded-lg">
+        {({ open }) => (
+          <>
+            <div className=" px-4 sm:px-6 lg:px-8">
+              <div className="flex h-16 items-center justify-between">
+                <div className="-mr-2 flex md:hidden">
+                  {/* Mobile menu button */}
+                  <Disclosure.Button
+                    onClick={() => setIsShowing((isShowing) => !isShowing)}
+                    className="inline-flex items-center justify-center rounded-md border border-white p-2 text-white hover:bg-gray-400 hover:text-white "
+                  >
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <FaBars className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <FaBars className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
+                </div>
+                <div className="flex items-center">
+                  <a href="/trang-nha-vuon" className="flex-shrink-0">
+                    <img
+                      className="h-11"
+                      src={require("../../assets/images/logo/fmp.png")}
+                      alt="FMP"
+                    />
+                  </a>
+                  <div className="hidden md:block">{/* Navbar */}</div>
+                </div>
+                <div className="md:block">
+                  <div className="ml-4 flex items-center md:ml-6">
+                    {/* Profile dropdown */}
+                    <Menu as="div" className="relative ml-3">
+                      <div>
+                        <Menu.Button className="flex max-w-xs items-center rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-1 ">
+                          <span className="sr-only">Open user menu</span>
+                          <img
+                            className="h-11 w-11 rounded-full"
+                            src={user.imageUrl}
+                            alt=""
+                          />
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          {userNavigation.map((item) => (
+                            <Menu.Item key={item.name}>
+                              {({ active }) => (
+                                <a
+                                  href={item.href}
+                                  className={classNames(
+                                    active ? "bg-gray-100" : "",
+                                    "block px-4 py-2 text-sm text-gray-700"
+                                  )}
+                                >
+                                  {item.name}
+                                </a>
+                              )}
+                            </Menu.Item>
+                          ))}
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
+                    <a
+                      href="#"
+                      className="hidden block px-3 text-md text-black md:block"
                     >
-                      <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        <FaBars className="block h-6 w-6" aria-hidden="true" />
-                      ) : (
-                        <FaBars className="block h-6 w-6" aria-hidden="true" />
-                      )}
-                    </Disclosure.Button>
-                  </div>
-                  <div className="flex items-center">
-                    <a href="/trang-nha-vuon" className="flex-shrink-0">
-                      <img
-                        className="h-11"
-                        src={require("../../assets/images/logo/fmp.png")}
-                        alt="FMP"
-                      />
+                      {user.name}
                     </a>
-                    <div className="hidden md:block">{/* Navbar */}</div>
-                  </div>
-                  <div className="md:block">
-                    <div className="ml-4 flex items-center md:ml-6">
-                      {/* Profile dropdown */}
-                      <Menu as="div" className="relative ml-3">
-                        <div>
-                          <Menu.Button className="flex mr-2 max-w-xs items-center rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-1 ">
-                            <span className="sr-only">Open user menu</span>
-                            <img
-                              className="h-11 w-11 rounded-full"
-                              src={user.imageUrl}
-                              alt=""
-                            />
-                          </Menu.Button>
-                        </div>
-                        <Transition
-                          as={Fragment}
-                          enter="transition ease-out duration-100"
-                          enterFrom="transform opacity-0 scale-95"
-                          enterTo="transform opacity-100 scale-100"
-                          leave="transition ease-in duration-75"
-                          leaveFrom="transform opacity-100 scale-100"
-                          leaveTo="transform opacity-0 scale-95"
-                        >
-                          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {userNavigation.map((item) => (
-                              <Menu.Item key={item.name}>
-                                {({ active }) => (
-                                  <a
-                                    href={item.href}
-                                    className={classNames(
-                                      active ? "bg-gray-100" : "",
-                                      "block px-4 py-2 text-sm text-gray-700"
-                                    )}
-                                  >
-                                    {item.name}
-                                  </a>
-                                )}
-                              </Menu.Item>
-                            ))}
-                          </Menu.Items>
-                        </Transition>
-                      </Menu>
-                    </div>
+                    <Menu as="div" className="relative ml-3 hidden md:block">
+                      <div>
+                        <Menu.Button className="flex max-w-xs items-center rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-1 p-2 bg-white">
+                          <span className="sr-only">Open user menu</span>
+                          <BiMessageRoundedDetail
+                            className="block h-6 w-6"
+                            aria-hidden="true"
+                          />
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          {userNavigation.map((item) => (
+                            <Menu.Item key={item.name}>
+                              {({ active }) => (
+                                <a
+                                  href={item.href}
+                                  className={classNames(
+                                    active ? "bg-gray-100" : "",
+                                    "block px-4 py-2 text-sm text-gray-700"
+                                  )}
+                                >
+                                  {item.name}
+                                </a>
+                              )}
+                            </Menu.Item>
+                          ))}
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
+                    <Menu as="div" className="relative ml-3 hidden md:block">
+                      <div>
+                        <Menu.Button className="flex max-w-xs items-center rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-1 p-2 bg-white">
+                          <span className="sr-only">Open user menu</span>
+                          <MdOutlineNotificationsNone
+                            className="block h-6 w-6"
+                            aria-hidden="true"
+                          />
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          {userNavigation.map((item) => (
+                            <Menu.Item key={item.name}>
+                              {({ active }) => (
+                                <a
+                                  href={item.href}
+                                  className={classNames(
+                                    active ? "bg-gray-100" : "",
+                                    "block px-4 py-2 text-sm text-gray-700"
+                                  )}
+                                >
+                                  {item.name}
+                                </a>
+                              )}
+                            </Menu.Item>
+                          ))}
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
                   </div>
                 </div>
               </div>
-            </>
-          )}
-        </Disclosure>
-
-        <div className="flex md:m-4">
-          {/* SideBar */}
-          <Transition
-            show={isShowing}
-            enter="transition ease-in-out duration-300 transform"
-            enterFrom="-translate-x-full"
-            enterTo="translate-x-0"
-            leave="transition ease-in-out duration-300 transform"
-            leaveFrom="translate-x-0"
-            leaveTo="-translate-x-full"
-          >
-            <div
-              className={
-                !isShowing
-                  ? "bg-white hidden md:block h-screen  rounded-xl p-1"
-                  : "bg-white  h-screen  rounded-xl p-1"
-              }
-            >
-              <NavLink
-                activeClassName="active"
-                to="/trang-nha-vuon/trang-chu"
-                className="link none-decoration"
-              >
-                <div className="">
-                  <div className="icon-sidebar">
-                    <TbApps />
-                  </div>
-                  <div className="link_text">Trang chủ</div>
-                </div>
-              </NavLink>
-              <NavLink
-                activeClassName="active"
-                to="/trang-nha-vuon/san-pham"
-                className="link none-decoration"
-              >
-                <div className="">
-                  <div className="icon-sidebar">
-                    <BiBox />
-                  </div>
-                  <div className="link_text">Sản phẩm</div>
-                </div>
-              </NavLink>
-              <NavLink
-                activeClassName="active"
-                to="/trang-nha-vuon/san-luong"
-                className="link none-decoration"
-              >
-                <div className="">
-                  <div className="icon-sidebar">
-                    <TbDatabaseExport />
-                  </div>
-                  <div className="link_text">Sản lượng</div>
-                </div>
-              </NavLink>
-              <NavLink
-                activeClassName="active"
-                to="/trang-nha-vuon/don-hang"
-                className="link none-decoration"
-              >
-                <div className="">
-                  <div className="icon-sidebar">
-                    <BiFoodMenu />
-                  </div>
-                  <div className="link_text">Hóa đơn</div>
-                </div>
-              </NavLink>
-              <NavLink
-                activeClassName="active"
-                to="/trang-nha-vuon/doanh-thu"
-                className="link none-decoration"
-              >
-                <div className="">
-                  <div className="icon-sidebar">
-                    <BiBarChartSquare />
-                  </div>
-                  <div className="link_text">Doanh thu</div>
-                </div>
-              </NavLink>
-              <NavLink
-                activeClassName="active"
-                to="/trang-nha-vuon/ngan-hang"
-                className="link none-decoration"
-              >
-                <div className="">
-                  <div className="icon-sidebar">
-                    <GoCreditCard />
-                  </div>
-                  <div className="link_text">Ngân hàng</div>
-                </div>
-              </NavLink>
             </div>
-          </Transition>
-          <div className="w-full ml-4">
-            <main>{children}</main>
+          </>
+        )}
+      </Disclosure>
+
+      <div className="flex mt-4">
+        {/* SideBar */}
+        <Transition
+          show={isShowing}
+          enter="transition ease-in-out duration-300 transform"
+          enterFrom="-translate-x-full"
+          enterTo="translate-x-0"
+          leave="transition ease-in-out duration-300 transform"
+          leaveFrom="translate-x-0"
+          leaveTo="-translate-x-full"
+        >
+          <div className="back-ground-side hidden md:block h-screen rounded-lg p-1 w-24">
+            {sideBar.map((s, index) => (
+              <NavLink
+                activeClassName="active"
+                to={s.link}
+                className="link none-decoration "
+              >
+                <div className="text-center">
+                  <div className="container-icon">
+                    <div className="icon-sidebar">{s.icon}</div>
+                  </div>
+                  <div className="link_text">{s.name}</div>
+                </div>
+              </NavLink>
+            ))}
           </div>
+        </Transition>
+        <div className="w-full ml-4">
+          <main>{children}</main>
         </div>
       </div>
     </div>
--- src/pages/gardens/Dashboard/DashboardGarden.jsx
+++ src/pages/gardens/Dashboard/DashboardGarden.jsx
@@ -3,7 +3,38 @@ import React from "react";
 const DashboardGarden = () => {
   return (
     <div className="row">
-
+      <div className="col-md-3">
+        <div className="h-36 bg-element rounded-lg mt-2"></div>
+      </div>
+      <div className="col-md-3">
+        {" "}
+        <div className="h-36 bg-element rounded-lg mt-2"></div>
+      </div>
+      <div className="col-md-6">
+        {" "}
+        <div className="h-36 bg-element rounded-lg mt-2"></div>
+      </div>
+      <div className="col-md-3">
+        <div className="h-36 bg-element rounded-lg mt-2"></div>
+      </div>
+      <div className="col-md-3">
+        {" "}
+        <div className="h-36 bg-element rounded-lg mt-2"></div>
+      </div>
+      <div className="col-md-6">
+        {" "}
+        <div className="h-36 bg-element rounded-lg mt-2"></div>
+      </div> <div className="col-md-3">
+        <div className="h-36 bg-element rounded-lg mt-2"></div>
+      </div>
+      <div className="col-md-3">
+        {" "}
+        <div className="h-36 bg-element rounded-lg mt-2"></div>
+      </div>
+      <div className="col-md-6">
+        {" "}
+        <div className="h-36 bg-element rounded-lg mt-2"></div>
+      </div>
     </div>
   );
 };
