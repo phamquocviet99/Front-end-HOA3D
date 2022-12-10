7fedff22d9169f2c28187fb4051ac44537f5ac67
phamquocviet99 <phamquocviet1211999@gmail.com>
2022-12-10 08:26:20 +0700

    next

--- src/pages/gardens/Quantity/Components/OutOfStockProduct.jsx
+++ src/pages/gardens/Quantity/Components/OutOfStockProduct.jsx
@@ -1,3 +1,90 @@
+import { useState } from "react";
+import CardProductAdded from "../../PostProduct/Components/CardProductAdded";
+import CardOutOfStockProduct from "./CardOutOfStockProduct";
+
 export default function OutOfStockProduct(){
-    return
+    const [listProductAdmin, setListProductAdmin] = useState([
+        {
+          id: "1",
+          name: "Hoa sen",
+          avatar: "../../../assets/images/logo/fmp.png",
+          nameCategory: "Hoa",
+        },
+        {
+          id: "2",
+          name: "Hoa sen",
+          avatar: "../../../assets/images/logo/fmp.png",
+          nameCategory: "Hoa",
+        },
+        {
+          id: "3",
+          name: "Hoa sen",
+          avatar: "../../../assets/images/logo/fmp.png",
+          nameCategory: "Hoa",
+        },
+        {
+          id: "4",
+          name: "Hoa sen",
+          avatar: "../../../assets/images/logo/fmp.png",
+          nameCategory: "Hoa",
+        },
+        {
+          id: "5",
+          name: "Hoa sen",
+          avatar: "../../../assets/images/logo/fmp.png",
+          nameCategory: "Hoa",
+        },
+        {
+          id: "6",
+          name: "Hoa sen",
+          avatar: "../../../assets/images/logo/fmp.png",
+          nameCategory: "Hoa",
+        },
+        {
+          id: "7",
+          name: "Hoa sen",
+          avatar: "../../../assets/images/logo/fmp.png",
+          nameCategory: "Hoa",
+        },
+        {
+          id: "8",
+          name: "Hoa sen",
+          avatar: "../../../assets/images/logo/fmp.png",
+          nameCategory: "Hoa",
+        },
+        {
+          id: "7",
+          name: "Hoa sen",
+          avatar: "../../../assets/images/logo/fmp.png",
+          nameCategory: "Hoa",
+        },
+        {
+          id: "8",
+          name: "Hoa sen",
+          avatar: "../../../assets/images/logo/fmp.png",
+          nameCategory: "Hoa",
+        },
+      ]);
+      return (
+        <div className="card-element p-4">
+          <div className="row">
+            <div className="col-md-6">
+              <h4 className="text-zinc-500 text-base md:text-xl mt-1 mb-3 ml-2 font-medium">
+                Danh sách sản phẩm hết hàng
+              </h4>
+            </div>
+           
+          </div>
+    
+          <div>
+            <div className="row mt-3">
+              {listProductAdmin?.map((p, index) => (
+                <div className="col-md-2 mt-3">
+                  <CardOutOfStockProduct />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      );
 }
