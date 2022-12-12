c829277619e1c2aefe4702d94ba84bd202c91651
phamquocviet99 <phamquocviet1211999@gmail.com>
2022-12-12 07:47:16 +0700

    next

--- src/pages/gardens/Modal/ModalDetailOrder.jsx
+++ src/pages/gardens/Modal/ModalDetailOrder.jsx
@@ -24,38 +24,38 @@ export default function ModalDetailOrder({ visible, onClose }) {
               <p className="text-xl mb-0">Nhà vườn</p>
             </div>
             <div className="flex">
-              <p className=" text-slate-500 text-base font-bold mb-3 w-64">
+              <p className=" text-slate-500 text-sm md:text-base font-bold mb-3 w-64">
                 Tên chủ nhà vườn :
               </p>
 
-              <p className="text-slate-500 text-base font-medium mb-3 w-64">
+              <p className="text-slate-500 text-sm md:text-base font-medium mb-3 w-64">
                 Ba Garden
               </p>
             </div>
             <div className="flex">
-              <p className=" text-slate-500 text-base font-bold mb-3 w-64">
+              <p className=" text-slate-500 text-sm md:text-base font-bold mb-3 w-64">
                 Tên nhà vườn :
               </p>
 
-              <p className="text-slate-500 text-base font-medium mb-3 w-64">
+              <p className="text-slate-500 text-sm md:text-base font-medium mb-3 w-64">
                 Ba Vườn
               </p>
             </div>
             <div className="flex">
-              <p className=" text-slate-500 text-base font-bold mb-3 w-64">
+              <p className=" text-slate-500 text-sm md:text-base font-bold mb-3 w-64">
                 Số điện thoại :
               </p>
 
-              <p className="text-slate-500 text-base font-medium mb-3 w-64">
+              <p className="text-slate-500 text-sm md:text-base font-medium mb-3 w-64">
                 0909959619
               </p>
             </div>
             <div className="flex">
-              <p className=" text-slate-500 text-base font-bold mb-3 w-64">
+              <p className=" text-slate-500 text-sm md:text-base font-bold mb-3 w-64">
                 Địa chỉ :
               </p>
 
-              <p className="text-slate-500 text-base font-medium mb-3 w-64">
+              <p className="text-slate-500 text-sm md:text-base font-medium mb-3 w-64">
                 B12/20 Lê Sao
               </p>
             </div>
@@ -63,38 +63,38 @@ export default function ModalDetailOrder({ visible, onClose }) {
               <p className="text-xl mb-0">Cửa hàng</p>
             </div>
             <div className="flex">
-              <p className=" text-slate-500 text-base font-bold mb-3 w-64">
+              <p className=" text-slate-500 text-sm md:text-base font-bold mb-3 w-64">
                 Tên chủ cửa hàng :
               </p>
 
-              <p className="text-slate-500 text-base font-medium mb-3 w-64">
+              <p className="text-slate-500 text-sm md:text-base font-medium mb-3 w-64">
                 Minh Lan Shop
               </p>
             </div>
             <div className="flex">
-              <p className=" text-slate-500 text-base font-bold mb-3 w-64">
+              <p className=" text-slate-500 text-sm md:text-base font-bold mb-3 w-64">
                 Tên nhà vườn :
               </p>
 
-              <p className="text-slate-500 text-base font-medium mb-3 w-64">
+              <p className="text-slate-500 text-sm md:text-base font-medium mb-3 w-64">
                 Nguyễn Minh Lan
               </p>
             </div>
             <div className="flex">
-              <p className=" text-slate-500 text-base font-bold mb-3 w-64">
+              <p className=" text-slate-500 text-sm md:text-base font-bold mb-3 w-64">
                 Số điện thoại :
               </p>
 
-              <p className="text-slate-500 text-base font-medium mb-3 w-64">
+              <p className="text-slate-500 text-sm md:text-base font-medium mb-3 w-64">
                 0909656919
               </p>
             </div>
             <div className="flex">
-              <p className=" text-slate-500 text-base font-bold mb-3 w-64">
+              <p className=" text-slate-500 text-sm md:text-base font-bold mb-3 w-64">
                 Địa chỉ :
               </p>
 
-              <p className="text-slate-500 text-base font-medium mb-3 w-64">
+              <p className="text-slate-500 text-sm md:text-base font-medium mb-3 w-64">
                 1230 Đường 3/2, Quận 6
               </p>
             </div>
@@ -104,82 +104,82 @@ export default function ModalDetailOrder({ visible, onClose }) {
               <p className="text-xl mb-0">Chi tiết hóa đơn</p>
             </div>
             <div className="flex">
-              <p className=" text-slate-500 text-base font-bold mb-3 w-64">
+              <p className=" text-slate-500 text-sm md:text-base font-bold mb-3 w-64">
                 Tên sản phẩm
               </p>
 
-              <p className="text-slate-500 text-base font-medium mb-3 w-64">
+              <p className="text-slate-500 text-sm md:text-base font-medium mb-3 w-64">
                 Hoa Lan Trắng
               </p>
             </div>
             <div className="flex">
-              <p className=" text-slate-500 text-base font-bold mb-3 w-64">
+              <p className=" text-slate-500 text-sm md:text-base font-bold mb-3 w-64">
                 Ngày đặt hàng :
               </p>
 
-              <p className="text-slate-500 text-base font-medium mb-3 w-64">
+              <p className="text-slate-500 text-sm md:text-base font-medium mb-3 w-64">
                 22/11/2022
               </p>
             </div>
             <div className="flex">
-              <p className=" text-slate-500 text-base font-bold mb-3 w-64">
+              <p className=" text-slate-500 text-sm md:text-base font-bold mb-3 w-64">
                 Số lượng :
               </p>
 
-              <p className="text-slate-500 text-base font-medium mb-3 w-64">200kg</p>
+              <p className="text-slate-500 text-sm md:text-base font-medium mb-3 w-64">200kg</p>
             </div>
             <div className="flex">
-              <p className=" text-slate-500 text-base font-bold mb-3 w-64">
+              <p className=" text-slate-500 text-sm md:text-base font-bold mb-3 w-64">
                 Tổng tiền :
               </p>
 
-              <p className="text-slate-500 text-base font-medium mb-3 w-64">
+              <p className="text-slate-500 text-sm md:text-base font-medium mb-3 w-64">
                 12.000.000đ
               </p>
             </div>
 
             <div className="flex">
-              <p className=" text-slate-500 text-base font-bold mb-3 w-64 ">
+              <p className=" text-slate-500 text-sm md:text-base font-bold mb-3 w-64 ">
                 Tên chành xe :
               </p>
 
-              <p className="text-slate-500 text-base font-medium mb-3 w-64">
+              <p className="text-slate-500 text-sm md:text-base font-medium mb-3 w-64">
                 Thành Bưởi
               </p>
             </div>
             <div className="flex">
-              <p className=" text-slate-500 text-base font-bold mb-3 w-64">
+              <p className=" text-slate-500 text-sm md:text-base font-bold mb-3 w-64">
                 Mã giao hàng :
               </p>
 
-              <p className="text-slate-500 text-base font-medium mb-3 w-64">
+              <p className="text-slate-500 text-sm md:text-base font-medium mb-3 w-64">
                 TB22033
               </p>
             </div>
             <div className="flex">
-              <p className=" text-slate-500 text-base font-bold mb-3 w-64">
+              <p className=" text-slate-500 text-sm md:text-base font-bold mb-3 w-64">
                 Điện thoại chành xe :
               </p>
 
-              <p className="text-slate-500 text-base font-medium mb-3 w-64">
+              <p className="text-slate-500 text-sm md:text-base font-medium mb-3 w-64">
                 090962255
               </p>
             </div>
             <div className="flex">
-              <p className=" text-slate-500 text-base font-bold mb-3 w-64">
+              <p className=" text-slate-500 text-sm md:text-base font-bold mb-3 w-64">
                 Địa chỉ nhận hàng
               </p>
 
-              <p className="text-slate-500 text-base font-medium mb-3 w-64">
+              <p className="text-slate-500 text-sm md:text-base font-medium mb-3 w-64">
                 1234 đường 3/2 Quận 6
               </p>
             </div>
             <div className="flex">
-              <p className=" text-slate-500 text-base font-bold mb-3 w-64">
+              <p className=" text-slate-500 text-sm md:text-base font-bold mb-3 w-64">
                 Ngày dự kiến nhận hàng
               </p>
 
-              <p className="text-slate-500 text-base font-medium mb-3 w-64">
+              <p className="text-slate-500 text-sm md:text-base font-medium mb-3 w-64">
                 22/12/2022
               </p>
             </div>
