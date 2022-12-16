bbc8aa826cd88680743a74c53ce5f52206b3914c
phamquocviet99 <phamquocviet1211999@gmail.com>
2022-12-17 05:51:53 +0700

    next

--- src/components/TabHeader/TabHeader.jsx
+++ src/components/TabHeader/TabHeader.jsx
@@ -5,14 +5,14 @@ export default function TabHeader(props) {
   const [isActiveTab, setIsActiveTab] = useState(1);
   return (
     <div className="card-element w-full shadow-md">
-      <div className=" w-full rounded-xl md:rounded-b-none block md:flex items-center pl-5 pt-1 bg-slate-50 pb-3 md:bg-transparent md:pb-0">
+      <div className=" w-full rounded-xl md:rounded-b-none block sm:flex items-center pl-5 pt-1 pb-3 sm:bg-transparent sm:pb-0">
         {props.tabName.map((t, index) => (
           <div key={index}>
             <div
               onClick={() => setIsActiveTab(t.id)}
               className={
                 isActiveTab === t.id
-                  ? "element-tab-product-active md:border-b-4 md:border-blue-500 md:text-blue-500"
+                  ? "element-tab-product-active sm:border-b-4 sm:border-blue-500 sm:text-blue-500"
                   : "element-tab-product text-gray-400 border-b-4 border-transparent hover:border-b-4 hover:border-blue-400 hover:text-blue-400"
               }
             >
