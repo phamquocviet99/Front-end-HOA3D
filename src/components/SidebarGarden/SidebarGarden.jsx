e21660ad8a8b1701c058d6ce577cf8b2b1b9d42d
phamquocviet99 <phamquocviet1211999@gmail.com>
2022-12-16 21:18:29 +0700

    next

--- src/components/SidebarGarden/SidebarGarden.jsx
+++ src/components/SidebarGarden/SidebarGarden.jsx
@@ -83,7 +83,7 @@ const SidebarGarden = ({ children }) => {
           onClick={() => setIsShowing((isShowing) => !isShowing)}
           className="absolute z-20 flex justify-start background-side-phone h-full w-full  xl:hidden"
         >
-          <div className="relative flex w-3/5   md:w-1/3 back-ground-side pt-10 pb-6 h-full ">
+          <div className="relative flex w-2/3 sm:3/5  md:w-1/3 back-ground-side pt-10 pb-6 h-full ">
             {/* <div className="flex justify-center max-w3/4 min-w-2/4 back-ground-side pt-10 pb-6 h-full "> */}
             <div className="w-full pl-3 pr-6">
               <div className="flex justify-between items-center px-2 w-full mb-4">
